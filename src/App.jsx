b965d968962b8177f7b7c3f0bf228459090e89c8
Roberto D Diaz O <rddiazo75@gmail.com>
2024-01-14 15:01:32 -0400

    Users CRUD 1.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,52 +4,41 @@ import useFetch from './hooks/useFetch'
 import UserCard from './components/UserCard'
 import FormUser from './components/FormUser'
 
+
 function App() {
 
   const [userUpdate, setUserUpdate] = useState()
-  const [isFormClose, setIsFormClose] = useState(true)
 
   const baseUrl = 'https://users-crud.academlo.tech'
-  const [users, getUsers, createUser, deleteUser, updateUser] = useFetch(baseUrl)
+  const [ users, getUsers, createUser, deleteUser, updateUser  ] = useFetch(baseUrl)
 
   useEffect(() => {
     getUsers()
   }, [])
 
-  const handleOpenForm = () => {
-    setIsFormClose(false)
-  }
-
-  console.log(users)
   return (
-    <div >
+    <div>
       <h1>User CRUD</h1>
-      <button onClick={handleOpenForm} >Open Form</button>
-      <div className={`form__container ${isFormClose && 'form__close'}`} >
-        <FormUser
-          createUser={createUser}
-          userUpdate={userUpdate}
-          updateUser={updateUser}
-          setUserUpdate={setUserUpdate}
-          setIsFormClose={setIsFormClose}
-        />
-      </div>
+      <FormUser
+      createUser={createUser}
+      userUpdate={userUpdate}
+      updateUser={updateUser}
+      setUserUpdate={setUserUpdate}
+      />
       <div>
         {
-          users?.map(user => (
-            <UserCard
+          users?.map(user =>(
+              <UserCard 
               key={user.id}
               user={user}
               deleteUser={deleteUser}
               setUserUpdate={setUserUpdate}
-              setIsFormClose={setIsFormClose}
-
-            />
-          ))
+              />
+            ))
         }
       </div>
     </div>
-  )
+    )
 }
 
 export default App
--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -1,74 +1,56 @@
 import { useEffect } from "react"
 import { useForm } from "react-hook-form"
-import './styles/FormUser.css'
 
-const FormUser = ({ createUser, userUpdate, updateUser, setUserUpdate, setIsFormClose }) => {
- 
-    const { handleSubmit, register, reset }= useForm()
+const FormUser = ({ createUser, userUpdate, updateUser, setUserUpdate }) => {
 
-    useEffect(() => {
-      reset(userUpdate)
+    const { handleSubmit, register, reset } = useForm()
+
+    useEffect(()  => {
+        reset(userUpdate)
     }, [userUpdate])
- 
+
     const submit = data => {
         if(userUpdate) {
             // Update
-            updateUser(updateUser.id, data)
+            updateUser(userUpdate.id, data)
             setUserUpdate()
         } else {
             // Crear
             createUser(data)
         }
-        
         reset({
-            email:'',
+            email: '',
             password: '',
             first_name: '',
             last_name: '',
-            birthday: ''            
+            birthday: ''
         })
-        setUserUpdate()
-        }
+    }
 
-        const handleExit = () => {
-          setIsFormClose(true)
-          reset({
-            email:'',
-            password: '',
-            first_name: '',
-            last_name: '',
-            birthday: ''            
-        })
-        }
-        
-
-  return (
-    <form className="form" onSubmit={handleSubmit(submit)}>
-      <div className="form__x" onClick={handleExit}>x</div>
-      <h2 className="form__title">{ userUpdate ? 'Update User' : 'Create new user'}</h2>
-      <label className="form__label"> 
-        <span className="form__field__name">Email </span>
-        <input className="form__field" {...register('email')} type="email" placeholder="user@email.com"/>
-      </label>
-      <label className="form__label">
-        <span className="form__field__name">Password </span>
-        <input className="form__field" {...register('password')} type="password" />
-      </label>
-      <label className="form__label">
-        <span className="form__field__name">First name </span>
-        <input className="form__field" {...register('first_name')} type="text" />
-      </label>
-      <label className="form__label">
-        <span className="form__field__name">Last name </span>
-        <input className="form__field"  {...register('last_name')} type="text" />
-      </label>
-      <label class Name="form__label">
-        <span className="form__field__name">Birthday </span>
-        <input className="form__field" {...register('birthday')} type="date" />
-      </label>
-      <button className="form__btn">{ userUpdate ? 'Update' : 'Create' }</button>
-    </form>
-  )
+    return (
+        <form onSubmit={handleSubmit(submit)}>
+            <label>
+                <span>Email</span>
+                <input {...register('email')} type="email" />
+            </label>
+            <label>
+                <span>Password</span>
+                <input {...register('password')} type="password" />
+            </label>
+            <label>
+                <span>First name</span>
+                <input {...register('first_name')} type="text" />
+            </label>
+            <label>
+                <span>Last name</span>
+                <input {...register('last_name')} type="text" />
+            </label>
+            <label>
+                <span>Birthday</span>
+                <input {...register('birthday')} type="date" />
+            </label>
+            <button>{ userUpdate ? 'Update' : 'Create' }</button>
+        </form>
+    )
 }
-
 export default FormUser
